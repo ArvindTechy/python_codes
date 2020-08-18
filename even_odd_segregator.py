873fa7e91a830d729da1fd15ff0de385e8fcf84f
ArvindTechy <53651431+ArvindTechy@users.noreply.github.com>
2020-08-19 00:10:04 +0530

    Update even_odd_segregator.py

--- even_odd_segregator.py
+++ even_odd_segregator.py
@@ -1,10 +1,10 @@
-def func(*args):
-    odd = []
-    even = []
+def func(*args):#args will take multiple input and excute them 
+    odd = []#blank list
+    even = []#blank list
     for no in args:
-        if no % 2 == 0:
-            even.append(no)
+        if no % 2 == 0:#divide by 2 if remainder 0 then continue or go to else
+            even.append(no)#number will be added to even list
         else:
-            odd.append(no)
-    print ("Even nos.=",even)
-    print ("Odd nos.=",odd) 
+            odd.append(no)#number will be added to odd list
+    print ("Even nos.=",even)#prints the list of even numbers
+    print ("Odd nos.=",odd) #prints the list of odd numbers
