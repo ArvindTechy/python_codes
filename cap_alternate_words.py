44d9f2e985ec17a60bdaf84b4eb42957cbb3ec75
ArvindTechy <53651431+ArvindTechy@users.noreply.github.com>
2020-08-18 23:50:21 +0530

    Update cap_alternate_words.py

--- cap_alternate_words.py
+++ cap_alternate_words.py
@@ -1,13 +1,13 @@
 def myfunc(words):
-    new_word=[]
+    new_word=[]#empty list 
     n=0 #n=1 will captalize from first word
     for word in words:
         if n==0:
-            nw=word.lower()
-            new_word.append(nw)
-            n=1  
+            nw=word.lower() #to lower the alphabet
+            new_word.append(nw)#adds the word into the list
+            n=1  #to switch the next word
         else:
-            nw=word.upper()
-            new_word.append(nw)
-            n=0
-    return''.join(new_word)
+            nw=word.upper()#to capitalize the alphabet 
+            new_word.append(nw)#adds the word into the list
+            n=0 #to switch the next word
+    return''.join(new_word)#to combine the aplhabets into original word
